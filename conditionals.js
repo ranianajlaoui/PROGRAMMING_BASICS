5653b009b1d123b62d86a9ba2b3d0f96086e24d8
unknown <ranianajlaoui69@gmail.com>
2024-09-26 12:19:52 -0700

    conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+var min_age = 10;
+var min_height = 42;
+Stretch Feature 1
+if (height>=42 && age>=10)
+    {  "Get on that ride, kiddo!"}
+else{
+     "Sorry kiddo. Maybe next year."
+}
+Stretch Feature 2
+if ( height>=42 || age>=10) {
+     "Get on that ride, kiddo!"
+
+}
+else{ "Sorry kiddo. Maybe next year."}
