aa787a40c3c56ed859052408a1c57aaea819aacc
unknown <ranianajlaoui69@gmail.com>
2024-09-25 17:26:17 -0700

    code.js

---
+++ code.js
@@ -0,0 +1,7 @@
+// count likes- what would this variable do? 
+var likeCount = 0;
+// increase likes  - what would be the function of this code block?
+function increaseLikes() {
+    // likecont function - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
