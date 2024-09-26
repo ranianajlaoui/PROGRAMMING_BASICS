d557486a648bf98fd29d1696861bf74c3469ec04
unknown <ranianajlaoui69@gmail.com>
2024-09-26 10:22:41 -0700

    snippingtools

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+//["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 1234]
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+//["broccoli","ice cream","hashbrowns"]
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+//["Bambi", "Beetlejuice", "Toy Story","Zorro"]
