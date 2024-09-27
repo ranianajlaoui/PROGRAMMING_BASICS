c99e848c1c33ac8767e717410db2b7993a3087aa
unknown <ranianajlaoui69@gmail.com>
2024-09-27 12:46:49 -0700

    parameters.js

---
+++ parameters.js
@@ -0,0 +1,11 @@
+
+//////////////////////////
+function greet(name,time){
+        if (name == "Count Dooku"){
+            console.log("I'm coming for you, Dooku!")
+        } else {
+            console.log('good '+time+' ' +name)
+        }
+    }
+greet("anakin","morning")
+greet("Count Dooku","morning")
